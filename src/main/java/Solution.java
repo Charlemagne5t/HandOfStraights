e32efa235f3e0f8dfe4d53f3fbaa864ff5620aa4
alek <wait82985@gmail.com>
2023-06-26 16:20:08 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,8 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        //TODO
+        int n = hand.length;
+        if (n % groupSize != 0) {
+            return false;
+        }
+        Arrays.sort(hand);
+        List<List<Integer>> listOfHands = new ArrayList<>();
+        for (int i = 0; i < n / groupSize; i++) {
+            listOfHands.add(new ArrayList<>());
+        }
 
-        return  false;
+        for (int i = 0; i < n; i++) {
+            int card = hand[i];
+            boolean inserted = false;
+            for (int j = 0; j < listOfHands.size(); j++) {
+                if (listOfHands.get(j).size() == 0) {
+                    listOfHands.get(j).add(card);
+                    inserted = true;
+                    break;
+                } else if (listOfHands.get(j).size() < groupSize) {
+                    if ((listOfHands.get(j).get(listOfHands.get(j).size() - 1) - card) == -1) {
+                        listOfHands.get(j).add(card);
+                        inserted = true;
+                        break;
+                    }
+                }
 
+            }
+            if (!inserted) return false;
+
+        }
+        return true;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -19,4 +19,33 @@ public class SolutionTest {
 
         Assert.assertFalse(actual);
     }
+
+    //8,10,12
+    @Test
+    public void isNStraightHandTest3(){
+        int[] hand = {8,10,12};
+        int groupSize = 3;
+        boolean actual = new Solution().isNStraightHand(hand, groupSize);
+
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void isNStraightHandTest4(){
+        int[] hand = {1,2};
+        int groupSize = 2;
+        boolean actual = new Solution().isNStraightHand(hand, groupSize);
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void isNStraightHandTest5(){
+        int[] hand = {2,3,1};
+        int groupSize = 3;
+        boolean actual = new Solution().isNStraightHand(hand, groupSize);
+
+        Assert.assertTrue(actual);
+    }
+
 }
